454f112d9af20a9585403c50acb9bffeca83a44b
gro-ser <seregaaa33@ya.ru>
2020-09-21 14:04:37 +0300

    Added HOME Driver

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,30 @@
 $global:esc=''
 $global:prompt='[1;33mPS$($PSVersionTable.PSVersion.Major) [32m$([Environment]::UserName)@$([Environment]::MachineName)[22;37m:[1;36m$pwd[m'
-function global:prompt{return (iex """$prompt$('>'*($nestedPromptLevel+1)) """)}
+
+function global:prompt
+{
+    $ExecutionContext.InvokeCommand.ExpandString($prompt) + ('>'*($nestedPromptLevel+1)) + " "
+}
 function global:Switch-PromptInversion
 {
-    if ($global:prompt.StartsWith('[7m'))
+    if ($global:prompt.StartsWith('[7m')) #[m
     {
         $global:prompt = $prompt.Substring(4)
     }
     else
     {
-        $global:prompt = "[7m$prompt"
+        $global:prompt = "[7m$prompt" #[m
     }
 }
 function global:Switch-PromptBold
 {
-    if ($global:prompt.Contains('[1'))
+    if ($global:prompt.Contains('[1')) #[m
     {
-        $global:prompt = $prompt -replace '\[1;(\d+)m','[$1m'
+        $global:prompt = $prompt -replace '\[1;(\d+)m','[$1m' #[m
     }
     else
     {
-        $global:prompt = $prompt -replace '\[(\d+)m', '[1;$1m'
+        $global:prompt = $prompt -replace '\[(\d+)m', '[1;$1m' #[m
     }
 }
+if ((New-PSDrive -Name % -PSProvider FileSystem -Root $HOME -Description 'Driver for Home path' -ErrorAction Ignore) -and ($pwd.Path -eq $HOME)) { Set-Location %: }
